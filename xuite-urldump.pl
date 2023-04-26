cffd3e713ddb39fca08e660e427553f835910de2
Gea-Suan Lin <gslin@gslin.org>
2023-04-26 18:13:36 +0800

    Add perl script to export urls from Xuite.

---
+++ xuite-urldump.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use WWW::Mechanize;
+
+INIT {
+    my $baseurl = shift @ARGV or die $!;
+
+    #
+    my $ua = WWW::Mechanize->new;
+
+    my $res = $ua->get($baseurl);
+
+    # Find max page (last page)
+    my $lastpage = 1;
+    foreach my $link ($ua->links) {
+        my $url = $link->url_abs;
+
+        if ($url =~ m{^\Q${baseurl}\E\?&p=(\d+)$}) {
+            $lastpage = $1 if int $1 > $lastpage;
+        }
+    }
+
+    # Loop to last page
+    for (my $page = 1; $page <= $lastpage; ) {
+        foreach my $link ($ua->links) {
+            my $url = $link->url_abs;
+
+            if ($url =~ m{^\Q${baseurl}\E/\d+$}) {
+                say "[*] Got ${url}";
+            }
+        }
+
+        $page++;
+        $ua->get($baseurl . '?&p=' . $page);
+    }
+}
+
+__END__
